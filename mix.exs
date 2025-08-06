3d85dd0dbd68cb966c88ea388e680ac1b7abc37f
Klaus Meyer <spam@klaus-meyer.net>
2025-08-06 11:37:29 +0000

    fix warning

--- mix.exs
+++ mix.exs
@@ -9,7 +9,8 @@ defmodule Filamentory.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      listeners: [Phoenix.CodeReloader]
     ]
   end
 
